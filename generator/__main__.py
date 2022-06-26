02712760537b0b57d16a34c8b79f5b230ccb0ce9
prateekb1912 <prateekb1912@gmail.com>
2022-06-26 10:30:47 +0530

    Initialize argparser to handle termnial-based ops

---
+++ generator/__main__.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+
+argparser = ArgumentParser(
+    prog='generator',
+    description="Generate image datasets from Google via your terminal"
+)
+
+reqd = argparser.add_argument_group('required_arguments')
+reqd.add_argument(
+    '-q', '--query',
+    required=True,
+    help='Query to search for images to be downloaded'
+)
+
+argparser.add_argument(
+    '-s', '--size',
+    help='Number of images to be downloaded in the dataset',
+    default=5000
+)
+
+argparser.add_argument(
+    '-p', '--path',
+    help='Path of dataset directory to store the images',
+    default='./dataset'
+)
+
+args = argparser.parse_args()
