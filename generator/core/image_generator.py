314fea1dbb28cd833bb5b369be1bcc8604c6cb76
prateekb1912 <prateekb1912@gmail.com>
2022-06-30 18:07:19 +0530

    Add progress bar over image scraper

--- generator/core/image_generator.py
+++ generator/core/image_generator.py
@@ -4,6 +4,7 @@ import os
 import requests
 import time
 
+from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -14,10 +15,9 @@ options = Options()
 options.add_argument('-profile')
 options.add_argument('/home/marvin/snap/firefox/common/.mozilla/firefox')
 
-def fetch_image_urls(query, max_links, driver, sleep_time = 1):
+def fetch_image_urls(query, max_links, driver):
     def scroll_to_end(driver):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_time)
     
     # Build the google query
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
@@ -28,39 +28,38 @@ def fetch_image_urls(query, max_links, driver, sleep_time = 1):
     image_count = 0
     results_start = 0
 
-    while image_count < max_links:
-        scroll_to_end(driver)
+    with tqdm(total=max_links) as pbar:
+        while image_count < max_links:
+            scroll_to_end(driver)
+            # Get all thumbanil images
+            thumbnail_res = driver.find_elements(By.CSS_SELECTOR, 'img.Q4LuWd')
+            num_results = len(thumbnail_res)
 
+            print(f"{results_start}:{num_results}")
 
-        # Get all thumbanil images
-        thumbnail_res = driver.find_elements(By.CSS_SELECTOR, 'img.Q4LuWd')
+            if num_results == results_start:
+                print("No more images found! Aborting extraction....")
+                return image_urls
 
-        num_results = len(thumbnail_res)
+            for img in thumbnail_res[results_start:num_results]:
+                try:
+                    img.click()
+                    pbar.update()
+                except Exception:
+                    continue
+                
+                actual_images = driver.find_elements(By.CSS_SELECTOR, 'img.n3VNCb')
 
-        print(f"Found: {num_results} search results. Extracting links from {results_start}:{num_results}")
+                for actual_img in actual_images:
+                    if actual_img.get_attribute('src') and 'http' in actual_img.get_attribute('src'):
+                        image_urls.add(actual_img.get_attribute('src'))
 
-        if num_results == results_start:
-            print("No more images found! Aborting extraction....")
-            return image_urls
-
-        for img in thumbnail_res[results_start:num_results]:
-            try:
-                img.click()
-            except Exception:
-                continue
+                image_count = len(image_urls)
+                if len(image_urls) >= max_links:
+                    print(f"Found: {len(image_urls)} image links, done!")
+                    break
             
-            actual_images = driver.find_elements(By.CSS_SELECTOR, 'img.n3VNCb')
-
-            for actual_img in actual_images:
-                if actual_img.get_attribute('src') and 'http' in actual_img.get_attribute('src'):
-                    image_urls.add(actual_img.get_attribute('src'))
-
-            image_count = len(image_urls)
-            if len(image_urls) >= max_links:
-                print(f"Found: {len(image_urls)} image links, done!")
-                break
-        
-        results_start = len(thumbnail_res)
+            results_start = len(thumbnail_res)
     
     return image_urls
 
@@ -93,7 +92,7 @@ def search_and_download(search_term, dataset_path = './dataset', number_images =
         os.makedirs(dataset_folder)
     
     with webdriver.Firefox(options=options) as driver:
-        res = fetch_image_urls(search_term, number_images, driver, 0.01)
+        res = fetch_image_urls(search_term, number_images, driver)
 
     for elem in res:
         persist_image(dataset_folder, elem)
